3e936ba4c614dce1bdf2d7b9ffc83c1fd357b879
Alan <spambox+github@zenteck.co.uk>
2019-11-11 15:59:18 +0000

    student

---
+++ student/specs/student_spec.rb
@@ -0,0 +1,41 @@
+require('minitest/autorun')
+require_relative('../student.rb')
+
+class TestStudent < Minitest::Test
+
+def test_create_student
+student = Student.new("Gordon", "G17")
+end
+
+def test_student_name
+  new_student = Student.new("Gordon", "G17")
+assert_equal("Gordon", new_student.student_name)
+end
+
+def test_cohort
+  new_student = Student.new("Gordon", "G17")
+  assert_equal("G17", new_student.cohort)
+end
+
+def test_set_student_name
+  new_student = Student.new("Gordon", "G17")
+  new_student.set_student_name("Donna")
+  assert_equal("Donna", new_student.student_name)
+end
+
+def test_set_cohort
+  new_student = Student.new("Gordon", "G17")
+  new_student.set_cohort("G18")
+  assert_equal("G18", new_student.cohort)
+end
+
+def test_student_talk
+  new_student = Student.new("Gordon", "G17")
+  new_student.student_talk
+  assert_equal("I wish I was here.", new_student.student_talk)
+end
+
+
+
+
+end
---
+++ student/student.rb
@@ -0,0 +1,29 @@
+class Student
+
+def initialize(student_name, cohort)
+  @student_name = student_name
+  @cohort = cohort
+end
+
+def student_name
+  return @student_name
+end
+
+def cohort
+  return @cohort
+end
+
+def set_student_name(new_student_name)
+  @student_name = new_student_name
+end
+
+def set_cohort(new_cohort)
+  @cohort = new_cohort
+end
+
+def student_talk
+  return "I wish I was here."
+end
+
+
+end
